ac1ba6b29034e176c8094527d8a009fdae78b28e
kavil <kavillatoro316@gmail.com>
2025-07-06 16:12:53 -0500

    still nonfunctional, but have a much better idea of what I'm doing and what sorts of barriers theres going to be in webscrapping

--- redditScrapping/reddit.js
+++ redditScrapping/reddit.js
@@ -20,22 +20,34 @@ url = 'https://www.reddit.com/r/stocks/';
 
     let previousHeight;
 
-    let stopCount = 10;
+    let stopCount = 2;
+
+    /*
+
+        ok I get whats happening now, I can't get it done right now but notes for future me:
+        The posts are being kept as objects, in order to properly scrape reddit, fetch object, then query each object
+        or at least thats my understanding so far.
+        Thats why below it's not easy to just fetch, the elements are not being shown in plane html
+        but instead as objects.
+
+    */
 
     let scrollCount = 0;
-
+    console.log(testvar);
     do{ 
 
         scrollCount +=1;
 
         previousHeight = await page.evaluate('document.body.scrollHeight');
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-        await page.waitForTimeout(2000);
+
+        await page.waitForTimeout(5000);
+
     } while (await page.evaluate('document.body.scrollHeight') > previousHeight && scrollCount < stopCount);
 
     const redditPost = await page.$$eval('article', articles =>{
         articles.map(article=>{
-            const text = article.querySelector('a')?.innterText || "";
+            const text = article.innerText;
 
             return {
                 post : text
@@ -43,8 +55,8 @@ url = 'https://www.reddit.com/r/stocks/';
         });
     });
 
-    console.log(redditPost);
-
+    console.log(typeof(redditPost));
+    
     await page.pause();
 
     browser.close();
