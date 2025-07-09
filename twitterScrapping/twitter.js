cf779b98f6867e4207e09f5a91add46a21563c6d
kavil <kavillatoro316@gmail.com>
2025-07-09 02:30:38 -0500

    seperated how the details are grabbes, much better to parse out later

--- twitterScrapping/twitter.js
+++ twitterScrapping/twitter.js
@@ -21,33 +21,64 @@ async function twitterScrape(url) {
     let previousHeight;
 
     let seenTweets = new Set();
-    let allTweets = []
+    let allTweets = [];
 
     do {
         previousHeight = await page.evaluate('document.body.scrollHeight');
 
+        //do: document.querySelectorAll('article') this is recommended against, but works for now
+
         const tweets = await page.$$eval('article', articles =>
         articles.map(article =>{
 
-            const text = article.innerText;
+            const text = article.querySelector('[data-testid="tweetText"] span').innerText;
+            
+            //this section is dedicated to grabbing the metrics of the posts
+
+            let metricsSet = new Set();
+            let textMetrics = article.querySelectorAll('[data-testid="app-text-transition-container"] span');
+
+            textMetrics.forEach(node=>{
+                if(!metricsSet.has(node.innerText)){
+                    metricsSet.add(node.innerText);
+                }
+            })
+
+            let metricNames = ['Comments', 'Retweets', 'Likes', 'Views'];
+
+            textMetrics = Array.from(metricsSet);
+            
+            textMetrics = textMetrics.map((element, index) =>{
+                return `${metricNames[index]}: ${element}`;
+            });
+
+            //end metrics section
+
+            //grab images and videos below
+
             const img= Array.from(article.querySelectorAll('[data-testid="tweetPhoto"] img'))
             .map(img=>img.src);
             const video = Array.from(article.querySelectorAll('video')).map(video=>video.src);
 
             return {
-                text,
-                images: img,
+                Text : text,
+                Metrics : textMetrics,
+                Images: img,
                 videos: video
             };
 
         }));
 
+        //remove dup tweets due to some mishap while scrolling
+        //I'm not sure that this is necessary, but likely better
+        //and yes this is the only since of error check I have right now
+
         tweets.forEach(tweet=>{
-            if(!seenTweets.has(tweet.text)){
-                seenTweets.add(tweet.text);
+            if(!seenTweets.has(tweet.Text)){
+                seenTweets.add(tweet.Text);
                 allTweets.push(tweet);
             };
-        })
+        });
 
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
         await page.waitForTimeout(2000);
