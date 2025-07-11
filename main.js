f5e3db8ff29a3ffaef6d56afcc8cd446adf9b1a3
kavil <kavillatoro316@gmail.com>
2025-07-11 15:00:49 -0500

    added the exported files to scrape from one file isntead and to start begining to parse the information

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
+const twitterScrape = require('./twitterScrapping/twitter')
+const redditScrape = require('./redditScrapping/reddit');
+
+const twitterURL = 'https://x.com/home?lang=en';
+const redditURL = 'https://www.reddit.com/r/stocks/'
+
+twitterScrape(twitterURL);
+redditScrape(redditURL);
+
+
+
 //the combination and saving of all the different scrapping's
 //I should essentially be able to run and return all types of json, that will be saved
 // and organized by the date
