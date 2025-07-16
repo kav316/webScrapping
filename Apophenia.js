e521c6237e671d29a5959f9000283b9a3ad9b50f
kavil <kavillatoro316@gmail.com>
2025-07-16 16:23:30 -0500

    words great, small problem, because I was lazy when I first made the twitter bot its capturaing metrics as well in trying to find the patterns, and I don't have a good way to clip of the metrics, so hopefully this variance just falls off the most information I get

--- Apophenia.js
+++ Apophenia.js
@@ -3,6 +3,14 @@ const path = require('path');
 
 async function wordCount(givenPath){
 
+    const stopWords = new Set([
+        'a', 'an', 'the', 'is', 'are', 'was', 'were', 'to', 'of', 'in', 'on', 'for', 'with',
+        'and', 'or', 'but', 'at', 'by', 'it', 'this', 'that', 'from', 'as', 'be', 'has', 'have',
+        'had', 'do', 'does', 'did', 'so', 'if', 'out', 'up', 'down', 'i', 'you', 'he', 'she', 'they',
+        'we', 'my', 'your', 'our', 'their', 'not', 'me', 'his', 'her', 'them', 'its', 'what', 'why', 'will',
+        'which', 'when', 'than', 'going', 'could', 'after', 'how'
+    ]);
+
     let contentArray = [];
 
     if(fs.readdirSync(givenPath)){
@@ -11,22 +19,34 @@ async function wordCount(givenPath){
             const filePath = path.join(givenPath, file);
             const fileObj = fs.readFileSync(filePath, 'utf8');
             const jsonObj = JSON.parse(fileObj);
-            for(post of jsonObj){
-                post.text = post.Title;
-                delete post.Title;
-            }
             contentArray.push(jsonObj);
-            fs.writeFileSync(filePath, JSON.stringify(jsonObj, null, 2), 'utf8');
-            
-            
         }
     } else{
         console.log('[ERROR] dir does not exist');
     }
 
-    //for(x of contentArray){
-    //    console.log(x);
-    //}
+    let wordCount = {};
+
+    for(x of contentArray){
+        for(content of x){
+            if(content.text){
+                let words = content.text.toLowerCase().replace(/[^a-z0-9\s]/g, '').split(/\s+/);
+                for(let word of words){
+                    if (stopWords.has(word)) {
+                        continue;
+                    }
+                    wordCount[word] = (wordCount[word] || 0) + 1;
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+
+    const sortedWords = Object.entries(wordCount).sort((a,b)=> b[1]-a[1]);
+
+    console.log(sortedWords);
+
 
 };
 
